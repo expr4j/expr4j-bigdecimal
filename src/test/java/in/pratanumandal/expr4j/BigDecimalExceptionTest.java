172653afc37613dc63989a4f4c55348b5f0badfe
Pratanu Mandal <pratanu.mandal@gmail.com>
2023-08-25 14:29:34 -0700

    Initial code commit

---
+++ src/test/java/in/pratanumandal/expr4j/BigDecimalExceptionTest.java
@@ -0,0 +1,176 @@
+/**
+ * Copyright 2023 Pratanu Mandal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package in.pratanumandal.expr4j;
+
+import in.pratanumandal.expr4j.exception.Expr4jException;
+import in.pratanumandal.expr4j.expression.Expression;
+import in.pratanumandal.expr4j.impl.BigDecimalExpressionBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class BigDecimalExceptionTest {
+	
+	protected BigDecimalExpressionBuilder builder = new BigDecimalExpressionBuilder();
+	
+	@Test
+	public void test1() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test2() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("()");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test3() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("5 +() 6");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test4() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("5() + 6");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test5() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("5 (+) 6");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test6() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("5 (+) 6");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test7() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("5 6 +");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test8() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("5 x +");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test9() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("+ 5 x");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test10() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("5 + x");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test11() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("6 sin 5 *");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test12() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("6 + * 5");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test13() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("5 max(6 *)");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test14() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("5 deg max(5) +");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test15() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("(5 *) max(5) +");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test16() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("(5,5)");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test17() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("(2 + (3)");
+			expression.evaluate();
+		});
+	}
+	
+	@Test
+	public void test18() {
+		Assertions.assertThrows(Expr4jException.class, () -> {
+			Expression<BigDecimal> expression = builder.build("(2 + 3))");
+			expression.evaluate();
+		});
+	}
+
+}
